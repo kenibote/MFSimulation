fe89f10c8cf3b33a29389aec9fc551aa2ffaa55a
wangning <kenibote@sina.com>
2018-12-04 10:19:24 +0800

    backup8

--- src/tool/GenerateTimeLine.java
+++ src/tool/GenerateTimeLine.java
@@ -16,6 +16,7 @@ public class GenerateTimeLine {
 
 	// 2018-1-1 00:00:00
 	static long start_time = 1514736000000L;
+	static int EndDay = 31;
 
 	@Test
 	public void generateMEC_Arrange_Task() {
@@ -26,7 +27,7 @@ public class GenerateTimeLine {
 		// MEC arrange 任务从第3天开始，前2天要保证有充足的内容被上传
 		long time = start_time + 2 * 24 * 60 * 60 * 1000;
 
-		for (int day = 3; day <= 31; day++) {
+		for (int day = 3; day <= EndDay; day++) {
 			for (int hour = 0; hour < 24; hour++) {
 				Task task = new Task();
 				task.setDate(new Date(time));
@@ -56,7 +57,7 @@ public class GenerateTimeLine {
 		// MEC check 任务从第3天开始，前2天要保证有充足的内容被上传; 每隔5分钟检查一次
 		long time = start_time + 2 * 24 * 60 * 60 * 1000;
 
-		for (int day = 3; day <= 31; day++) {
+		for (int day = 3; day <= EndDay; day++) {
 			for (int hour = 0; hour < 24; hour++) {
 				for (int min = 0; min < 60; min += 5) {
 					Task task = new Task();
@@ -85,20 +86,66 @@ public class GenerateTimeLine {
 		Session session = DataBaseTool.getSession();
 		Transaction tx = session.beginTransaction();
 		// ------------------------------------------
-		// TODO
+		// 生成上传时间缓存
 		HashMap<Integer, TimePattern> timeMap = new HashMap<>();
 		for (int id = 1; id <= 34; id++) {
 			timeMap.put(id, session.get(TimePattern.class, id));
 		}
 
+		// 为每个creater生成上传task
 		for (int id = 1; id <= GenerateCreaterUser.TotalCreaterNumber; id++) {
+			Creater creater = session.get(Creater.class, id);
+			// 打印监测信息
+			System.out.println(creater.getCreaterId());
 
+			// 先产生每天上传多少个视频， 服从泊松分布
+			int[] uploadNumber = new int[EndDay + 1];
+			for (int day = 1; day <= EndDay; day++) {
+				uploadNumber[day] = getPossionVariable(creater.getUploadArrivalRate());
+			}
+
+			// 为每一天创建task
+			for (int day = 1; day <= EndDay; day++) {
+				String pix = "C_2018_01_" + getFormateNumber(day, 2) + "_" + creater.getCreaterName() + "_";
+
+				int daycount = 1;
+				// 如果该天有多个上传任务
+				while (uploadNumber[day]-- > 0) {
+					String contentName = pix + getFormateNumber(daycount++, 3);
+
+					Task task = new Task();
+					task.setPriority(TaskType.Upload.ordinal());
+					task.setTaskType(TaskType.Upload);
+					task.setDate(timeMap.get(creater.getTimePatternId()).getRandomTime(1, day));
+					task.setTime(task.getDate().getTime() + task.getPriority());
+					// 设置上传者信息
+					task.setUpload_id(creater.getCreaterId());
+					task.setUpload_content(contentName);
+					// 保存任务
+					session.save(task);
+				} // end while
+			} // end for
+
+			session.flush();
 		}
 
 		// ------------------------------------------
 		tx.commit();
 		session.close();
 		DataBaseTool.closeSessionFactory();
+	}
+
+	public static String getFormateNumber(int data, int len) {
+		int length = ("" + data).length();
+		String res = "";
+
+		len = len - length;
+		while (len-- > 0) {
+			res = res + "0";
+		}
+		res = res + data;
+
+		return res;
 	}
 
 	@SuppressWarnings({ "deprecation", "unchecked" })
